cb3254f7a391fba7d1671a10900807d0d4ccc3c1
Amarjeet <kumaramarjeet001@gmail.com>
2023-10-04 00:08:51 +0530

    * Generated student entity

---
+++ src/main/java/com/core/Student.java
@@ -0,0 +1,37 @@
+package com.core;
+
+public class Student {
+	private int StudentId ;
+	private String Address ;
+	private String Section ;
+	public int getStudentId() {
+		return StudentId;
+	}
+	public void setStudentId(int studentId) {
+		StudentId = studentId;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	public Student(int studentId, String address, String section) {
+		super();
+		StudentId = studentId;
+		Address = address;
+		Section = section;
+	}
+	public String getSection() {
+		return Section;
+	}
+	public void setSection(String section) {
+		Section = section;
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
