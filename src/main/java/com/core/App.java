57872a0c40f03ed87ae12a8e64b4ba7012fa183a
Amarjeet <kumaramarjeet001@gmail.com>
2023-10-05 23:29:40 +0530

    * Called method via annotation

--- src/main/java/com/core/App.java
+++ src/main/java/com/core/App.java
@@ -1,6 +1,7 @@
 package com.core;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -12,7 +13,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml") ;
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("config.xml") ;
+        context.registerShutdownHook() ;
          Student student1 = (Student) context.getBean("student1") ;
          System.out.println(student1);
     }
